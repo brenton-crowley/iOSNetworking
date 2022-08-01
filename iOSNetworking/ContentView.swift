eb89f4536abdb69c6c82f7893ff8dcc1cca0552c
Brent <brenton.crowley@gmail.com>
2022-08-02 09:35:42 +1000

    Initial Commit

---
+++ iOSNetworking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSNetworking
+//
+//  Created by Brent on 2/8/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSNetworking/iOSNetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSNetworkingApp.swift
+//  iOSNetworking
+//
+//  Created by Brent on 2/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct iOSNetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
