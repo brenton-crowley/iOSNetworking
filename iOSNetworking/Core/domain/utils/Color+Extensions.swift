d09b7b66c8724950c7ab6fbe2cc76ff86c04ea56
Brent <brenton.crowley@gmail.com>
2022-08-02 11:40:42 +1000

    add colors for app theme

---
+++ iOSNetworking/Core/domain/utils/Color+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  Color+Extensions.swift
+//  iOSNetworking
+//
+//  Created by Brent on 2/8/2022.
+//
+
+import SwiftUI
+
+extension Color {
+
+    static let themeAccent = Color("AccentColor")
+    static let themeBackground = Color("BackgroundColor")
+    static let themeForeground = Color("ForegroundColor")
+    static let themeSecondary = Color("SecondaryColor")
+    static let themeTertiary = Color("TertiaryColor")
+    
+}
